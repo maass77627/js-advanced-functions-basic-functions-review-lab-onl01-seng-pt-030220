fd7eb834548543fa70c8772976e545731b317afc
kelly r maass <kmaass77627@gmail.com>
2020-10-10 21:50:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ function saturdayFun(activity="roller-skate") {
 return `This Saturday, I want to ${activity}!`;
 }
 
-// this monday, i will go to the office
 
 function mondayWork(event="go to the office") {
   return `This Monday, I will ${event}.`;
+}
+
+function wrapAdjective() {
+  return function(string="special") {
+    
+  }
 }

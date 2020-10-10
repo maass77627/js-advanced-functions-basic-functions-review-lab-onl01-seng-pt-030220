e9d2ab1ff0d2fcd2c4faf42a23543a37ef3467d5
kelly r maass <kmaass77627@gmail.com>
2020-10-10 21:48:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code here
+
+function saturdayFun(activity="roller-skate") {
+//console.log(`This Saturday, I want to ${phrase}!`)
+return `This Saturday, I want to ${activity}!`;
+}
+
+// this monday, i will go to the office
+
+function mondayWork(event="go to the office") {
+  return `This Monday, I will ${event}.`;
+}
